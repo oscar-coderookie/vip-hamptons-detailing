02053848d01de966ce598f6a62c25c7fd24ebd3e
unknown <industrialdesignoscar@gmail.com>
2024-02-27 22:32:51 -0500

    Fix errors

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -6,46 +6,40 @@ const AboutUs = () => {
   return (
     <div className="aboutus">
       <h3 className="aboutus__subtitle">our team</h3>
-      <div className="aboutus__block1">
-        <div className="aboutus__block">
-          <div className="aboutus__info">
-            <img
-              src="https://firebasestorage.googleapis.com/v0/b/vip-hamptons-detailing.appspot.com/o/boats%2Faboutus-desktop.jpg?alt=media&token=977a517a-e014-4c45-98ca-85ae6b9c8a7c"
-              alt="aboutus"
-              className="aboutus__image"
-            />
-            <p className="aboutus__text">
-              <b> Hamptons VIP Car and Boat Detailing</b> has been providing
-              superior detail service to Hamptons residents for the last 11
-              years.
-              <br />
-              <br />
-              We are located in <b>East Hampton</b> , but cover a wide area,
-              travelling to our clients’ homes wherever and whenever we are
-              needed.
-              <br />
-              <br />
-              We have a mobile workshop, which makes it possible to get
-              impeccable same-day service right in your own garage, backyard, or
-              boat dock.
-              <br />
-              <br />
-              Our team of friendly professionals takes pride in the convenience
-              and perfectionism of our services.
-              <br />
-              <br />
-              We strive to provide our customers with the best possible care for
-              their cars, boats, and RVs.
-              <br />
-              <br />
-              We can make your old car or boat look like it did the day you
-              drove it home from the showroom!
-              <br />
-              <br />
-              <b> Maintain and protect the beauty of your cars and boats!</b>
-            </p>
-          </div>
-        </div>
+      <div className="aboutus__block">
+      <img
+            src="https://firebasestorage.googleapis.com/v0/b/vip-hamptons-detailing.appspot.com/o/boats%2Faboutus-desktop.jpg?alt=media&token=977a517a-e014-4c45-98ca-85ae6b9c8a7c"
+            alt="aboutus"
+            className="aboutus__image"
+          />
+          <p className="aboutus__text">
+            <b> Hamptons VIP Car and Boat Detailing</b> has been providing
+            superior detail service to Hamptons residents for the last 11 years.
+            <br />
+            <br />
+            We are located in <b>East Hampton</b> , but cover a wide area,
+            travelling to our clients’ homes wherever and whenever we are
+            needed.
+            <br />
+            <br />
+            We have a mobile workshop, which makes it possible to get impeccable
+            same-day service right in your own garage, backyard, or boat dock.
+            <br />
+            <br />
+            Our team of friendly professionals takes pride in the convenience
+            and perfectionism of our services.
+            <br />
+            <br />
+            We strive to provide our customers with the best possible care for
+            their cars, boats, and RVs.
+            <br />
+            <br />
+            We can make your old car or boat look like it did the day you drove
+            it home from the showroom!
+            <br />
+            <br />
+            <b> Maintain and protect the beauty of your cars and boats!</b>
+          </p>
       </div>
       <h3 className="aboutus__subtitle">our mission:</h3>
       <PageBlock
@@ -66,7 +60,7 @@ const AboutUs = () => {
         }
       />
       <PageBlock
-      bckColor="#ebeded"
+        bckColor="#ebeded"
         description={
           "today is through the roof. Its so important to keep up with the maintenance of your vehicles. Here in EAST HAMPTON NY. it is amazing and beautiful but comes with a price. The suns heat and UV rays, salt Air and water, storms snow and everyday driving. We strive to help you keep up the value of your car and boat by providing the absolute best car detailing and boat detailing experience and protection."
         }
@@ -76,12 +70,12 @@ const AboutUs = () => {
       />
       <h3 className="aboutus__subtitle">our vision:</h3>
       <PageBlock
-      bckColor="#ebeded"
+        bckColor="#ebeded"
         description={
           "today is through the roof. Its so important to keep up with the maintenance of your vehicles. Here in EAST HAMPTON NY. it is amazing and beautiful but comes with a price. The suns heat and UV rays, salt Air and water, storms snow and everyday driving. We strive to help you keep up the value of your car and boat by providing the absolute best car detailing and boat detailing experience and protection."
         }
         image2Url={
-          "https://firebasestorage.googleapis.com/v0/b/vip-hamptons-detailing.appspot.com/o/cars%2Fmision%20y%20vision%2FMesa%20de%20trabajo%201%20copia.jpg?alt=media&token=45f2392b-4429-4228-b619-a9bae0722b0d"
+          "https://firebasestorage.googleapis.com/v0/b/vip-hamptons-detailing.appspot.com/o/cars%2Fmision%20y%20vision%2FMesa%20de%20trabajo%201.jpg?alt=media&token=9d158836-a042-466d-8214-8879651f370b"
         }
       />
     </div>
